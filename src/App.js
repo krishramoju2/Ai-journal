9a9aae2cac40a72c7cd17eb3e0a069fec58ce343
krishramoju2 <futureai.incorporated@gmail.com>
2025-06-24 22:26:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from "react";
-import JournalForm from "./JournalForm";
-import JournalTimeline from "./JournalTimeline";
-import "./index.css";
+import React from 'react';
+import JournalForm from './JournalForm';
+import JournalTimeline from './JournalTimeline';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>📝 AI-Powered Journal</h1>
-      <JournalForm />
-      <JournalTimeline />
-    </div>
-  );
-}
+const App = () => (
+  <div style={{ maxWidth: '800px', margin: 'auto', padding: '2rem' }}>
+    <h1>AI Journal App</h1>
+    <JournalForm />
+    <hr />
+    <JournalTimeline />
+  </div>
+);
 
 export default App;
